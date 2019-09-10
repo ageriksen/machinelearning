ad6c4fd50625c4e6eb25524a0ef38343a25b3eef
andersgeriksen <andersge92@gmail.com>
2019-09-10 08:41:18 +0200

    initial setup, project 1

--- hw/hw1/e2.py
+++ hw/hw1/e2.py
@@ -1,27 +1,13 @@
 # version 0.1 execrcise 2 homework 1
 """ 
-imports taken from liquid drop example on slides from regression. link under 
-task 1.
-"""
-import pandas as pd
-import matplotlib.pyplot as plt
-from IPython.display import display
-import os
-
-project_root_dir = "results"
-figure_id = "results/figurefiles"
-data_id = "datafiles"
-if not os.path.exists(project_root_dir):
-    os.mkdir(project_root_dir)
-
-""" 
 generate dataset for func. y(x), x \in [0, 1] defined by random uniform numbers.
 y is a quadratic polynomial, w/ stochastic noise from normal distribution N(0,1). 
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 x = np.random.rand(100, 1)
-y = 5*x^2 + 0.1*np.random.randn(100, 1)
+y = 5*x*x + 0.1*np.random.randn(100, 1)
 
 """ 
 1.
@@ -30,4 +16,26 @@ code generated using information found here.
 
 compute parameterization of data set fitting 2.order polynomial.
 """
+X = np.c_[np.ones(len(x)), x, x**2]
 
+beta = np.linalg.inv( X.T @ X ) @ X.T @ y
+
+y_pred = X @ beta
+
+
+""" 
+MSE( y_pred, y) 
+
+plt.scatter(x, y, label='true')
+plt.scatter(x, y_pred, label='predicted')
+plt.legend()
+plt.show()
+"""
+MSE = 1/len(y_pred) * np.linalg.norm(y - y_pred)**2
+print("MSE is: ", MSE)
+
+#plot
+plt.scatter(x, y, label='true')
+plt.scatter(x, y_pred, label='predicted')
+plt.legend()
+plt.show()
