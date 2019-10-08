4695238638396348d5cdda0ae3b7fb911a39b03b
andersgeriksen <andersge92@gmail.com>
2019-10-08 09:52:21 +0200

    finally did the kfold cross validation

--- proj/proj1/src/frankeols.py
+++ proj/proj1/src/frankeols.py
@@ -17,6 +17,27 @@ def Frankefunction(x, y):
     term3   =   0.5   *np.exp(    -0.25*(9*x - 7)**2  -   0.25*(9*y - 3)**2   )
     term4   =  -0.2   *np.exp(    -(9*x - 4)**2       -   (9*y - 7)**2        )
     return term1 + term2 + term3 + term4
+
+def Designmatrix(x, y, n=5):
+    """ 
+    create a design matrix dependent on the polynomial grade you want, with a base of 3.
+    want the collumns of X to be [1, x, y, x^2, xy, y^2, x^3, x^2y, xy^2, y^3]
+    and so on. 
+    """
+    if len(x.shape) > 1:
+        x = np.ravel(x)
+        y = np.ravel(y)
+
+    N = len(x)
+    l = int( (n+1)*(n+2)/2 )    # nr. of elements in beta
+    X = np.ones((N,l))
+
+    for i in range(1, n+1):
+        q = int( (i)*(i+1)/2 )
+        for k in range(i+1):
+            X[:, q+k] = x**(i-k) * y**k
+    
+    return X
 #   /////// !Functions   /////// 
 
 #   ///////   make random data   ///////   
@@ -65,28 +86,25 @@ vari    =   []
 zshape = zarr.reshape(-1,1)
 for deg in degrees:
     ztest_pred  =   np.empty(   (testarrsze, k)  )
+    ztests  =   np.empty(   (testarrsze, k)  )
+
     j   =   0
-    model   =   make_pipeline( PolynomialFeatures(degree=deg), LinearRegression( fit_intercept=False ) )
+    X = Designmatrix(rowarr, colarr, deg) 
+    for traininds, testinds in kfold.split(X):
 
-    for traininds, testinds in kfold.split(rowarr):
-
-        coltrain    =   colarr[traininds]
-        rowtrain    =   rowarr[traininds]
         ztrain      =   zarr[traininds]
+        ztest       =   zarr[testinds]
 
-        coltest     =   colarr[testinds]
-        rowtest     =   rowarr[testinds]
-        ztest       =   zshape[testinds]
+        Xtrain  =   X[traininds]
+        Xtest   =   X[testinds]
         
-        #print( '\n===================\ncoltrain.shape: ', coltrain.shape, '\nrowtrain.shape: ', rowtrain.shape, '\nztrain.shape: ', ztrain.shape, '\n=====================\n')
-        
-        model.fit(np.column_stack((coltrain, rowtrain)), ztrain)
-        matpred =   model.predict(np.column_stack((coltest, rowtest)))
-        ztest_pred[:,j] =   matpred.ravel()
+        beta = np.linalg.inv(Xtrain.T @ Xtrain) @ Xtrain.T @ ztrain
+        ztest_pred[:,j] =   Xtest @ beta
+        ztests[:,j] =   ztest
         j+=1
 
-    error_test      =   np.mean(    np.mean((ztest - ztest_pred)**2, axis=1, keepdims=True) )
-    bias_test       =   np.mean(    (ztest - np.mean(ztest_pred, axis=1, keepdims=True))**2 )
+    error_test      =   np.mean(    np.mean((ztests - ztest_pred)**2, axis=1, keepdims=True) )
+    bias_test       =   np.mean(    (ztests - np.mean(ztest_pred, axis=1, keepdims=True))**2 )
     variance_test   =   np.mean(    np.var(ztest_pred, axis=1, keepdims=True)               )
     err.append(error_test)
     bi.append(bias_test)
@@ -95,24 +113,11 @@ for deg in degrees:
 
 plt.figure()
 plt.plot(degrees, err, label='training set')
+plt.plot(degrees, bi, label='bias')
+plt.plot(degrees, vari, label='variance')
+#plt.plot(degrees, bi+vari, 'o', label='bias + variance')
 plt.xlabel('complexity')
 plt.ylabel('MSE')
 plt.title('OLS resampling k-fold CV')
 plt.legend()
-
-
-#fig = plt.figure()
-#
-#ax      =   fig.add_subplot(1, 2, 1, projection='3d')
-#surf    =   ax.plot_surface(
-#            rowmat, colmat, zmat, cmap=cm.coolwarm, linewidth=0, antialiased=False   )
-#fig.colorbar(surf, shrink=0.5, aspect=5)
-#plt.title('Measurement')
-#
-#matpred.reshape(rowmat.shape)
-#ax      =   fig.add_subplot(1, 2, 2, projection='3d')
-#surf    =   ax.plot_surface(
-#            rowmat, colmat, matpred, cmap=cm.coolwarm, linewidth=0, antialiased=False   )
-#fig.colorbar(surf, shrink=0.5, aspect=5)
-#plt.title('OLS fit')
 plt.show()
